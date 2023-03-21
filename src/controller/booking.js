708e5698a0cfe605798c68b77de5b8d85fcca909
shahjalal <lx.shahjalal@gmail.com>
2023-03-21 18:57:24 +0600

    fix

--- src/controller/booking.js
+++ src/controller/booking.js
@@ -1,23 +1,6 @@
-const Booking = require("../Models/Booking");
+const Booking = require('../Models/Booking');
 
 const booking = {};
-
-{
-    "packageId": "63d6859faffda9096738413f",
-    "brideName": "bobie",
-    "groomName": "leone",
-    "time": "19:52",
-    "contact": "01451245688",
-    "packageName": "Top photographer combo",
-    "packageAmount": "19999",
-    "advanceAmount": "5000",
-    "eventDate": "2023-03-31",
-    "transactionID": "20068563",
-    "accountNumber": "6539632",
-    "email": "wedding@gmail.com",
-    "venueDetails": "iccb"
-}
-
 
 booking.addBooking = async (req, res, next) => {
     try {
@@ -28,4 +11,4 @@ booking.addBooking = async (req, res, next) => {
     }
 };
 
-module.exports = booking
+module.exports = booking;
