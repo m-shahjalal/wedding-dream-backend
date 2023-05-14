29844768fb5f85cfb2901ff58ce97159d0d79d37
shahjalal <lx.shahjalal@gmail.com>
2023-05-14 14:15:58 +0600

    add portfolio

--- src/Models/Team.js
+++ src/Models/Team.js
@@ -1,13 +1,20 @@
-const { model, Schema } = require('mongoose')
+const { model, Schema } = require('mongoose');
 
-const memberSchema = new Schema({
-    name: { type: String, required: true, trim: true },
-    designation: { type: String, default: 'Photographer', trim: true },
-    image: { type: String, required: true },
-    social: {
-        facebook: String, linkedin: String, instagram: String, twitter: String
-    }
-}, { timestamps: true })
+const memberSchema = new Schema(
+    {
+        name: { type: String, required: true, trim: true },
+        designation: { type: String, default: 'Photographer', trim: true },
+        image: { type: String, required: true },
+        social: {
+            facebook: String,
+            linkedin: String,
+            instagram: String,
+            twitter: String,
+            portfolio: String,
+        },
+    },
+    { timestamps: true }
+);
 
-const Team = model('Team', memberSchema)
-module.exports = Team
+const Team = model('Team', memberSchema);
+module.exports = Team;
